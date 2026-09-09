7b1970d02670b3e14231d50c621deccd32613482
arshiaizd <izdyaria@gmail.com>
2026-09-09 19:58:29 +0000

    feat: add repeated multiplication power operation

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,4 +8,12 @@ public class Calculator {
     public int divide(int first, int second) {
         return first / second;
     }
+
+    public int power(int base, int exponent) {
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
 }
